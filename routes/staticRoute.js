7e6d7bdf2a92fed1eb31c42b33e4fec94b1ec869
MOHIT DAVAR <mohitdavar2004@gmail.com>
2024-10-19 17:38:27 +0530

    Down

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const { giveKey } = require("../controllers/giveKey.js")
-router.route("/")
-    .get((req, res) => {
-        res.render("index")
-    })
+// router.route("/")
+//     .get((req, res) => {
+//         res.render("index")
+//     })
 router.route("/key")
     .get(giveKey)
 router.route("/easy")
